ab0906980c0d36ee9e456bb491bae9fb44b0a2f7
Matvey <sinema3d00@gmail.com>
2024-09-01 10:19:16 +0300

    different types of injection

--- DIvsIOCvsDIP/Program.cs
+++ DIvsIOCvsDIP/Program.cs
@@ -1,12 +1,67 @@
-﻿var stars = new GitHubService().GetStars("throw");
+﻿var stars = new GitHubServiceDI().GetStars("throw");
 
 
 
+#region DIP vs DI vs IOC classes
+//DI - the bigger object don't rely on concrete smaller object - they use abstraction
+//IOC - giving some control over application or part to framework or container
+//
+internal class GitHubServiceDI(IGithubClient gitHubClient)
+{                                         //ctor injection
+    private IGithubClient _gitHubClient = gitHubClient; //DI
 
-internal class GitHubService
-{
     public int GetStars(string repoName)
     {
-
+        return _gitHubClient.GetRepo(repoName).stars;//DI
     }
 }
+
+internal class GitHubServiceSetter
+{                                         //ctor injection
+    private IGithubClient _gitHubClient; //DI
+
+    //setter injection
+    public void SetGithubClient(IGithubClient gitHubClient)
+    {
+        _gitHubClient = gitHubClient;
+    }
+
+    public int GetStars(string repoName)
+    {
+        return _gitHubClient.GetRepo(repoName).stars;//DI
+    }
+}
+internal class GitHubServiceInterface : IGithubClientSetter
+{
+    private IGithubClient _gitHubClient;
+
+    public void SetGithubClient(IGithubClient gitHubClient)
+    {
+        _gitHubClient = gitHubClient;
+    }
+
+    public int GetStars(string repoName)
+    {
+        return _gitHubClient.GetRepo(repoName).stars;//DI
+    }
+}
+
+internal interface IGithubClientSetter
+{
+    void SetGithubClient(IGithubClient githubClient);
+}
+
+internal interface IGithubClient //DI
+{
+    (string repoName, int stars) GetRepo(string repoName);
+}
+
+internal class GithubClient : IGithubClient //DI
+{
+   public (string repoName, int stars) GetRepo(string repoName)
+    {
+        return (repoName, repoName.Length);
+    }
+}
+
+#endregion
